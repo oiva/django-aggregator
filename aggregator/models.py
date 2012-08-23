2e49b5558ad8af1bd57556bdc64a8140fce67f93
Oiva Eskola <oiva.eskola@gmail.com>
2012-08-23 16:58:26 +0300

    unicode fix

--- aggregator/models.py
+++ aggregator/models.py
@@ -83,7 +83,7 @@ class Feed(models.Model):
                     self.entries.create(title=title, link=link, summary=summary,\
                         content=content, guid=guid, date=date_modified, image=image)
                 except DatabaseError:
-                    print 'fail: %s, %s, %s, %s' % (title, link, guid, image)
+                    print u'fail: %s, %s, %s, %s' % (title, link, guid, image)
 
 
     def parse_image(self, content):
